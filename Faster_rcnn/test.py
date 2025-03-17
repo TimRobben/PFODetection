23ca27074540974a597861a0202e4c473812d665
TimRobben <t.a.a.robben@gmail.com>
2025-03-17 16:39:42 +0100

    start on faster rcnn

---
+++ Faster_rcnn/test.py
@@ -0,0 +1,9 @@
+import torch
+import torchvision
+import detectron2
+
+print("PyTorch Version:", torch.__version__)
+print("TorchVision Version:", torchvision.__version__)
+print("CUDA Version:", torch.version.cuda)
+print(detectron2.__version__)
+print("Detectron2 Installed Successfully!")
